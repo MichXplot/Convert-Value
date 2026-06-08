b9a0b736cea2aeddaf3bbc931f9bf2aa52b3e8c5
Ilya <ilazadiraka@gmail.com>
2026-06-08 19:01:30 +0300

    Добавил новые валюты и обновил базу данных

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 curs = {
-    "RUB": ["в‚Ѕ", "ru"],
+    "RUB": ["₽", "ru"],
     "USD": ["$", "us"],
-    "EUR": ["в‚¬", "eu"],
-    "CNY": ["ВҐ", "cn"],
-    "KZT": ["в‚ё", "kz"]
+    "EUR": ["€", "eu"],
+    "CNY": ["¥", "cn"],
+    "KZT": ["₸", "kz"],
+    "GBP": ["£", "gb"],
+    "JPY": ["¥", "jp"],
+    "AED": ["د.إ", "ae"],
+    "BYN": ["Br", "by"]
 }
 
 def get_rate(cur):
@@ -29,7 +33,17 @@ def get_rate(cur):
             return res[0]
     except:
         pass
-    fb = {"USD": 95.0, "EUR": 105.0, "CNY": 13.0, "KZT": 0.2, "RUB": 1.0}
+    fb = {
+        "RUB": 1.0,
+        "USD": 95.0,
+        "EUR": 105.0,
+        "CNY": 13.0,
+        "KZT": 0.2,
+        "GBP": 125.0,
+        "JPY": 0.65,
+        "AED": 26.0,
+        "BYN": 29.0
+    }
     return fb.get(cur, 1.0)
 
 @app.get("/", response_class=HTMLResponse)
